e1b03cb52ef1a2facd10e0701b8dde3139523307
David Garcia <garcia.david@buhlschools.org>
2026-01-12 11:03:30 -0700

    Turret

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,126 +4,145 @@
 
 package frc.robot;
 
-import edu.wpi.first.hal.FRCNetComm.tResourceType;
-import edu.wpi.first.hal.HAL;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkBase.PersistMode;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.math.MathUtil;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
- * project.
- */
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
 
-  private RobotContainer m_robotContainer;
+    // ---------------- CAN IDs ----------------
+    private static final int TURRET_ID = 5;
+    //private static final int TOP_SHOOTER_ID = 10;
+    //private static final int BOTTOM_SHOOTER_ID = 11;
+    //private static final int FEEDER_ID = 12;
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any
-   * initialization code.
-   */
-  @Override
-  public void robotInit() {
-    // Instantiate our RobotContainer. This will perform all our button bindings,
-    // and put our
-    // autonomous chooser on the dashboard.
-    m_robotContainer = new RobotContainer();
+    // ---------------- Motors ----------------
+    private  SparkMax turretMotor;
+    //private  SparkMax topShooter;
+    //private  SparkMax bottomShooter;
+    //private  SparkMax feederMotor;
 
-    // Used to track usage of the KitBot code, please do not remove
-    HAL.report(tResourceType.kResourceType_Framework, 9);
-  }
+    // ---------------- Limelight ----------------
+    private NetworkTable limelight;
 
-  /**
-   * This function is called every 20 ms, no matter the mode. Use this for items
-   * like diagnostics
-   * that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and
-   * SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
+    @Override
+    public void robotInit() {
 
-  /** This function is called once each time the robot enters Disabled mode. */
-  @Override
-  public void disabledInit() {
-  }
+        turretMotor = new SparkMax(TURRET_ID, MotorType.kBrushless);
+        //topShooter = new SparkMax(TOP_SHOOTER_ID, MotorType.kBrushless);
+        //bottomShooter = new SparkMax(BOTTOM_SHOOTER_ID, MotorType.kBrushless);
+        //feederMotor = new SparkMax(FEEDER_ID, MotorType.kBrushless);
 
-  @Override
-  public void disabledPeriodic() {
-  }
+        
 
-  /**
-   * This autonomous runs the autonomous command selected by your
-   * {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+        // Bottom wheel inverted so wheels spin opposite directions
+        //bottomShooter.setInverted(true);
 
-    // schedule the autonomous command (example)
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.schedule();
+        limelight = NetworkTableInstance.getDefault().getTable("limelight");
+
+        // ---------------- SmartDashboard ----------------
+        SmartDashboard.putNumber("Turret kP", 0.02);
+        SmartDashboard.putNumber("Turret Max Speed", 0.4);
+        SmartDashboard.putNumber("Turret Deadband", 1.0);
+
+        SmartDashboard.putNumber("Shooter Base RPM", 3000);
+        SmartDashboard.putNumber("Shooter Angle Mult", 4.0);
+        SmartDashboard.putNumber("Shooter Alt Angle Mult", 7.0);
+        SmartDashboard.putNumber("Special Tag ID", 7);
+        SmartDashboard.putNumber("Shooter RPM Tolerance", 100);
+
+        SmartDashboard.putNumber("Feeder Speed", 0.5);
     }
-  }
 
-  /** This function is called periodically during autonomous. */
-  @Override
-  public void autonomousPeriodic() {
-  }
+    @Override
+    public void teleopPeriodic() {
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+        // ---------------- LIMELIGHT DATA ----------------
+        double tx = limelight.getEntry("tx").getDouble(0.0);
+        double ty = limelight.getEntry("ty").getDouble(0.0);
+        double tv = limelight.getEntry("tv").getDouble(0.0);
+        double tagID = limelight.getEntry("tid").getDouble(-1);
+
+        if (tv < 1.0) {
+            turretMotor.stopMotor();
+            //topShooter.stopMotor();
+            //bottomShooter.stopMotor();
+            //feederMotor.stopMotor();
+            return;
+        }
+
+        // ---------------- TURRET AIMING ----------------
+        double turretKP = SmartDashboard.getNumber("Turret kP", 0.02);
+        double turretMaxSpeed = SmartDashboard.getNumber("Turret Max Speed", 0.4);
+        double turretDeadband = SmartDashboard.getNumber("Turret Deadband", 1.0);
+
+        boolean turretReady;
+
+        if (Math.abs(tx) < turretDeadband) {
+            turretMotor.stopMotor();
+            turretReady = true;
+        } else {
+            double speed = MathUtil.clamp(tx * turretKP, -turretMaxSpeed, turretMaxSpeed);
+            turretMotor.set(speed);
+            turretReady = false;
+        }
+
+        // ---------------- DISTANCE CALC ----------------
+        double cameraHeight = 24.0; // inches
+        double tagHeight = 60.0;    // inches (same for all tags)
+        double cameraAngle = 30.0;  // degrees
+
+        double distance =
+                (tagHeight - cameraHeight) /
+                Math.tan(Math.toRadians(cameraAngle + ty));
+
+        // ---------------- SHOOTER ANGLE SELECTION ----------------
+        //double baseRPM = SmartDashboard.getNumber("Shooter Base RPM", 3000);
+        double normalMult = SmartDashboard.getNumber("Shooter Angle Mult", 4.0);
+        double altMult = SmartDashboard.getNumber("Shooter Alt Angle Mult", 7.0);
+        double specialTag = SmartDashboard.getNumber("Special Tag ID", 7);
+
+        double angleMultUsed =
+                (tagID == specialTag) ? altMult : normalMult;
+
+       // double rpmOffset = distance * angleMultUsed;
+
+        //double bottomTargetRPM = baseRPM + rpmOffset;
+        //double topTargetRPM = baseRPM - rpmOffset;
+
+        //bottomShooter.set(Math.min(bottomTargetRPM / 5700.0, 1.0));
+        //topShooter.set(Math.min(topTargetRPM / 5700.0, 1.0));
+
+        // ---------------- SHOOTER READY ----------------
+        //double rpmTolerance = SmartDashboard.getNumber("Shooter RPM Tolerance", 100);
+
+        //double topRPM = topShooter.getEncoder().getVelocity();
+        //double bottomRPM = bottomShooter.getEncoder().getVelocity();
+
+        //boolean shooterReady =
+                //Math.abs(topRPM - topTargetRPM) < rpmTolerance &&
+                //Math.abs(bottomRPM - bottomTargetRPM) < rpmTolerance;
+
+        // ---------------- AUTO FIRE ----------------
+        //double feederSpeed = SmartDashboard.getNumber("Feeder Speed", 0.5);
+
+       // if (turretReady && shooterReady) {
+            //feederMotor.set(feederSpeed);
+       // } else {
+            //feederMotor.stopMotor();
+       // }
+
+        // ---------------- DASHBOARD DEBUG ----------------
+        SmartDashboard.putNumber("Distance", distance);
+        SmartDashboard.putNumber("AprilTag ID", tagID);
+        SmartDashboard.putNumber("Angle Mult Used", angleMultUsed);
+        SmartDashboard.putBoolean("Turret Ready", turretReady);
+        //SmartDashboard.putBoolean("Shooter Ready", shooterReady);
     }
-  }
-
-  /** This function is called periodically during operator control. */
-  @Override
-  public void teleopPeriodic() {
-  }
-
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
-
-  /** This function is called periodically during test mode. */
-  @Override
-  public void testPeriodic() {
-  }
-
-  /** This function is called once when the robot is first started up. */
-  @Override
-  public void simulationInit() {
-  }
-
-  /** This function is called periodically whilst in simulation. */
-  @Override
-  public void simulationPeriodic() {
-  }
 }
